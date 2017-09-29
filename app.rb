ef5f4f0c741e82efc388917ec90fd02bf62db770
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-29 09:41:59 -0700

    Add route to view list of brands and form to add brands

--- app.rb
+++ app.rb
@@ -8,3 +8,22 @@ get('/') do
   @section = 'home'
   erb(:index)
 end
+
+get('/brands') do
+  @brands = Brand.all
+  erb(:brands)
+end
+
+get('/brand_form') do
+  erb(:brand_form)
+end
+
+post('/add_brand') do
+  brand = Brand.new({name: params.fetch("name"), price: params.fetch("price")})
+  if brand.save
+    redirect '/brands'
+  else
+    @error_type = brand
+    erb(:errors)
+  end
+end
