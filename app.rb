2b8a880dca3a4a790fb08b086f8d0e771cf4f642
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-29 09:58:19 -0700

    Add route to view list of stores and form to add stores.

--- app.rb
+++ app.rb
@@ -10,20 +10,44 @@ get('/') do
 end
 
 get('/brands') do
+  @section = 'brands'
   @brands = Brand.all
   erb(:brands)
 end
 
 get('/brand_form') do
+  @section = 'add-brand'
   erb(:brand_form)
 end
 
 post('/add_brand') do
+  @section = 'brands'
   brand = Brand.new({name: params.fetch("name"), price: params.fetch("price")})
   if brand.save
     redirect '/brands'
   else
     @error_type = brand
+    erb(:errors)
+  end
+end
+
+get('/stores') do
+  @section = 'stores'
+  @stores = Store.all
+  erb(:stores)
+end
+
+get('/store_form') do
+  erb(:store_form)
+end
+
+post('/add_store') do
+  @section = 'add-store'
+  store = Store.new({name: params.fetch("name")})
+  if store.save
+    redirect '/stores'
+  else
+    @error_type = store
     erb(:errors)
   end
 end
